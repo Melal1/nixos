8eb44573a3a846d6c42ea48ccdc78457215d2bd9
melal <example@example.com>
2025-10-26 08:08:12 +0300

    Uploaded Pfp

--- modules/home/assests/default.nix
+++ modules/home/assests/default.nix
@@ -4,4 +4,8 @@
     source = config.lib.file.mkOutOfStoreSymlink ./wallpapers;
     recursive = true;
   };
+  home.file."Pictures/Pfp/" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./Pfp;
+    recursive = true;
+  };
 }
