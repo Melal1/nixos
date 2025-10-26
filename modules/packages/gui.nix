a4b54d56021fb438b6a736f7dd58c866d8805f0f
melal <example@example.com>
2025-10-26 13:11:08 +0300

    Cleanup GUI packages and enable Nix store auto-optimization

--- modules/packages/gui.nix
+++ modules/packages/gui.nix
@@ -4,7 +4,6 @@
     waybar # Status bar for Wayland
     eww # Custom widgets for the desktop
     wlogout # Logout menu for Wayland
-    dunst # Lightweight notification daemon
     papirus-icon-theme
     hyprpicker # Color picker for Wayland
     # (discord.override {
--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,19 +1,14 @@
 {
   imports = [
-
-
     ./bootloader.nix
     ./base.nix
     ./networking.nix
     ./virtualisation.nix
     ./services.nix
-
-
-
-
   ];
 
+  nix.settings.auto-optimise-store = true;
 
 
 
-}
+    }
