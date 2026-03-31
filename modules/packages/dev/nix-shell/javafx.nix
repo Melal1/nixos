14c47a7286973a856ab76160c90425e29a61ad39
melal <melal.1@hotmail.com>
2026-03-31 03:05:10 +0300

    javafx first fix

--- modules/packages/dev/nix-shell/javafx.nix
+++ modules/packages/dev/nix-shell/javafx.nix
@@ -1,8 +1,26 @@
 { pkgs }:
 
+let
+  libs = [
+    # I tried moving some of the libraries below here to
+    # exclude them from $LD_LIBRARY_PATH.
+  ];
+  ld-lib-path-libs = [
+    # fatal error if the following libs are removed from $LD_LIBRARY_PATH:
+    pkgs.xorg.libXtst
+    pkgs.glib
+    # non-fatal error (backtrace displayed, but GUI still opens) if
+    # the following libs are removed from $LD_LIBRARY_PATH:
+    pkgs.xorg.libXxf86vm
+    pkgs.libGL
+  ];
+in
 pkgs.mkShell {
-  packages = [ pkgs.jdk21 pkgs.openjfx ];
+  packages = [
+    pkgs.jdk
+    pkgs.gradle
+  ] ++ libs ++ ld-lib-path-libs;
   shellHook = ''
-    export JAVAFX_HOME=${pkgs.openjfx}
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath ld-lib-path-libs}
   '';
 }
